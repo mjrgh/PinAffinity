bd1e4590a50cee49f5be2f3175583b9708283386
Michael Roberts <mjr_@hotmail.com>
2018-02-14 04:41:57 +0000

    Auto build number/date generation; add /Minimize startup option

---
+++ PinAffinity/Version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Major and minor version number.  When displaying the
+// version, we also add a generated build number, so we
+// don't bother with a third dotted part; the build number
+// serves that role more effectively for an open source
+// project like this that doesn't have a formal release
+// cycle.
+#define VSN_MAJOR   1
+#define VSN_MINOR   0
